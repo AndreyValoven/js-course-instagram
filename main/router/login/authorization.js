66bf441fd1a0e9a67714163e5f34115140c54240
AndyScorp <scorpion.and@ukr.net>
2018-03-09 20:17:34 +0200

    check email in authorization

--- main/router/login/authorization.js
+++ main/router/login/authorization.js
@@ -1,38 +1,49 @@
 const express = require('express');
-const User = require('./../../models/user');
 const jwt = require('jsonwebtoken');
+
+const User = require('./../../models/user');
+const valideteEmail = require('./../../functions/validate_email');
 
 const authoriztion = express.Router();
 
-authoriztion.post('/', (req, res) => {
-    console.log(req.body);
-    const body = req.body;
-    if(
-        typeof body.email === 'undefined' ||
-        typeof body.pwd === 'undefined' ||
-        body.email.replace(/ /, '') === '' ||
-        body.pwd.replace(/ /, '') === ''
-    ) {
-        return res.status(500).json({
-            error: 'empty values'
-        });
-    }
-    User.findOne({ email: body.email })
-        .then(user => {
-            console.log(user);
-            if(user == null || user.pwd !== body.pwd) {
-                res.sendStatus(404);
-            } else {
-                let token = jwt.sign({ user: user ,}, 'secret');
-                res.type('json');
-                res.status(200).json({token});
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            res.sendStatus(404);
-        });
-});
+authoriztion.post('/',
+    (req, res, next) => {
+        const body = req.body;
+        if(
+            typeof body.email === 'undefined' ||
+            typeof body.pwd === 'undefined' ||
+            body.email.replace(/ /, '') === '' ||
+            body.pwd.replace(/ /, '') === '' ||
+            !valideteEmail(body.email)
+        ) {
+            res.status(500).json({
+                error: 'empty values or not corect email'
+            });
+        } else {
+            next();
+        }
+    },
+    (req, res) => {
+        console.log(req.body);
+        const body = req.body;
+        User.findOne({ email: body.email })
+            .then(user => {
+                if(user == null || user.pwd !== body.pwd) {
+                    res.status(404).json({
+                        mes: 'wrong pasword or email'
+                    });
+                } else {
+                    let token = jwt.sign({ user: user ,}, 'secret');
+                    res.type('json');
+                    res.status(200).json({token});
+                }
+            })
+            .catch(error => {
+                res.status(504).json({
+                    error: error
+                });
+            });
+    });
 
 
 
