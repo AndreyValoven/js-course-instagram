d40a5490549c063cebce73b495708517f2a87fd9
AndyScorp <scorpion.and@ukr.net>
2018-03-10 15:12:10 +0200

    compare password by sync method

--- main/router/login/authorization.js
+++ main/router/login/authorization.js
@@ -28,22 +28,20 @@ authoriztion.post('/',
         const body = req.body;
         User.findOne({ email: body.email })
             .then(user => {
-                bcrypt.hash(body.pwd, user.pwd)
-                    .then(hash => {
-                        if(user == null || hash !== user.pwd) {
-                            res.status(404).json({
-                                mes: 'wrong pasword or email'
-                            });
-                        } else {
-                            let token = jwt.sign({ user: user ,}, 'secret');
-                            res.type('json');
-                            res.status(200).json({token});
-                        }
+                if(user == null || !bcrypt.compareSync(body.pwd, user.pwd)) {
+                    res.status(404).json({
+                        mes: 'wrong pasword or email'
                     });
+                } else {
+                    let token = jwt.sign({ user: user ,}, 'secret');
+                    res.type('json');
+                    res.status(200).json({token});
+                }
+
             })
-            .catch(error => {
+            .catch(err => {
                 res.status(504).json({
-                    error: error
+                    error: err
                 });
             });
     });
