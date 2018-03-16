dd945f70dc07b0da85aab3d33f28304bce0e0671
AndyScorp <scorpion.and@ukr.net>
2018-03-16 11:16:30 +0200

    fix user follower and following array

--- main/models/user.js
+++ main/models/user.js
@@ -1,15 +1,42 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 
-const user = mongoose.Schema({
+
+const User = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
     nick_name: String,
     email: String,
     pwd: String,
-    followers: Array,
-    following: Array,
+    followers: [
+        mongoose.Schema.Types.ObjectId
+    ],
+    following: [
+        mongoose.Schema.Types.ObjectId
+    ],
     avatar: String,
 });
 
-module.exports = mongoose.model('User', user);
+User.pre('save', function(next) {
+    let user = this;
+
+    if (!user.isModified('pwd')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(error, salt) {
+        if (error) return next(error);
+
+        user.pwd = bcrypt.hashSync(user.pwd, salt);
+        next();
+    });
+});
+
+User.methods.comparePwd = function(pwd, cd) {
+    bcrypt.compare(pwd, this.pwd, function(err, isMatch) {
+        if (err) return cd(err);
+        cd(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', User);
