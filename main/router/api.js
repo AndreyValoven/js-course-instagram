9f23c39a75a5141814249fa6899e5155ebadec2c
AndyScorp <scorpion.and@ukr.net>
2018-03-16 12:53:48 +0200

    create rout for get user

--- main/router/api.js
+++ main/router/api.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const api = require('express').Router();
 
 const registration = require('./login/registration');
 const authoriztion = require('./login/authorization');
-const varyfiToken = require('./../functions/verufi_token');
 const email = require('./check/email');
 const nickName = require('./check/nick_name');
-
-let api = express.Router();
 
 api.use('/registration', registration);
 api.use('/authoriztion', authoriztion);
@@ -15,16 +11,7 @@ api.use('/check/email', email);
 api.use('/check/nick_name', nickName);
 
 
-api.use('/user',varyfiToken, (req, res) => {
-    let token = req.token;
-    jwt.verify(token, 'secret', (err, data) => {
-        if(!err) {
-            res.json(data);
-        } else {
-            res.sendStatus(403);
-        }
-    });
-});
+api.use('/user', require('./user'));
 
 
 module.exports = api;
---
+++ main/router/user.js
@@ -0,0 +1,53 @@
+const user = require('express').Router();
+const varyfiToken = require('./../functions/verify_token');
+// const jwt = require('jsonwebtoken');
+
+const User = require('./../models/user');
+// const varyfiToken = require('./../functions/verufi_token');
+
+user.get('/:id', varyfiToken,(req, res) => {
+    // check if exists user in db
+    const id = req.params.id;
+    User.findOne({ _id: id })
+        .then(user => {
+            if ( user != null ) {
+                if (typeof req.id === 'object' && req.id !== user._id ) {
+                    let followerUser = false;
+                    console.log(req.id);
+                    for (const tokenId of user.followers) {
+                        tokenId.toString() === req.id.toString() ? followerUser = true : followerUser;
+                    }
+                    res.status(200).json({
+                        find: 'find',
+                        name: user.name,
+                        nick_name: user.nick_name,
+                        avatar: user.avatar,
+                        followers: user.followers.length,
+                        following: user.following.length,
+                        followUser: followerUser
+                    });
+                } else {
+                    res.status(200).json({
+                        name: user.name,
+                        nick_name: user.nick_name,
+                        avatar: user.avatar,
+                        followers: user.followers.length,
+                        following: user.following.length,
+                    });
+                }
+            } else {
+                res.status(404).json({
+                    error: 'not found'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+
+module.exports = user;
