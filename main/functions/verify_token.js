541acfe9b594ae41f06f2342bb68f37a58af9632
AndyScorp <scorpion.and@ukr.net>
2018-03-16 12:27:19 +0200

    rename file and and check if user id exist

---
+++ main/functions/verify_token.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/user');
+
+module.exports = (req, res, next) => {
+    const headerToken = req.headers['authorization'];
+    if(typeof headerToken !== 'undefined') {
+        const tokenArr = headerToken.split(' ');
+        const tokenKey = tokenArr[1];
+        let user = jwt.verify(tokenKey, process.env.SECRET_KEY);
+        console.info(user);
+        User.findOne({ _id: user.id })
+            .then(user => {
+                if ( user._id != null ) {
+                    req.id = user._id;
+                    next();
+                } else {
+                    next();
+                }
+            }).catch(() => {
+                next();
+            });
+    } else {
+        next();
+    }
+};
