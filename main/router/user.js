5c1fe0d7b034a3941f8bb69795112809bcf44ecf
AndyScorp <scorpion.and@ukr.net>
2018-03-26 14:11:25 +0300

    disable user update route

--- main/router/user.js
+++ main/router/user.js
@@ -2,7 +2,7 @@ const user = require('express').Router();
 const varyfiToken = require('./../functions/verify_token');
 // const jwt = require('jsonwebtoken');
 
-const checkValues = require('./../functions/check_values');
+// const checkValues = require('./../functions/check_values');
 const User = require('./../models/user');
 // const varyfiToken = require('./../functions/verufi_token');
 
@@ -49,51 +49,51 @@ user.get('/:id', varyfiToken,(req, res) => {
 });
 
 
-user.patch('/:id', varyfiToken,
-    (req, res) => {
-        const body = req.body;
-        const id = req.params.id;
-        if (req.id + '' !== id+ '') {
-            return res.status(403).json({ erorr: 'Forbidden'});
-        }
+// user.patch('/:id', varyfiToken,
+//     (req, res) => {
+//         const body = req.body;
+//         const id = req.params.id;
+//         if (req.id + '' !== id+ '') {
+//             return res.status(403).json({ erorr: 'Forbidden'});
+//         }
 
-        if (typeof req.id !== 'object') {
-            return res.status(503).json({
-                error: 'not exist'
-            });
-        }
+//         if (typeof req.id !== 'object') {
+//             return res.status(503).json({
+//                 error: 'not exist'
+//             });
+//         }
 
-        if (checkValues(body)) {
-            return res.status(400).json({
-                error: 'empty values or not corect email'
-            });
-        }
-        // check if exists uaser
-        User.findById( id,
-            function(error, user) {
-                if (error) {
-                    return res.status(501).json({
-                        error
-                    });
-                }
-                user.name = body.name;
-                user.nick_name = body.nick_name;
-                user.email = body.email;
-                user.pwd = body.pwd;
-                user.save()
-                    .then(user => {
-                        console.log(user);
-                        res.status(202).json({
-                            user,
-                        });
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        res.status(500).json({
-                            error
-                        });
-                    });
-            });
-    });
+//         if (checkValues(body)) {
+//             return res.status(400).json({
+//                 error: 'empty values or not corect email'
+//             });
+//         }
+//         // check if exists uaser
+//         User.findById( id,
+//             function(error, user) {
+//                 if (error) {
+//                     return res.status(501).json({
+//                         error
+//                     });
+//                 }
+//                 user.name = body.name;
+//                 user.nick_name = body.nick_name;
+//                 user.email = body.email;
+//                 user.pwd = body.pwd;
+//                 user.save()
+//                     .then(user => {
+//                         console.log(user);
+//                         res.status(202).json({
+//                             user,
+//                         });
+//                     })
+//                     .catch(error => {
+//                         console.log(error);
+//                         res.status(500).json({
+//                             error
+//                         });
+//                     });
+//             });
+//     });
 
 module.exports = user;
