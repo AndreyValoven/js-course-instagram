8eb113eeb65652c12237d476a50957c0a267eb46
AndyScorp <scorpion.and@ukr.net>
2018-04-02 22:03:35 +0300

    some chenges to image model and create route for image

--- main/models/image.js
+++ main/models/image.js
@@ -4,8 +4,13 @@ const image = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     user_id: mongoose.Schema.Types.ObjectId,
     url: String,
-    data: String,
-    likes: mongoose.Schema.Types.Array,
+    date: String,
+    likes: [
+        mongoose.Schema>Types.ObjectId
+    ],
+    tags: [
+        String
+    ]
 });
 
 module.exports = mongoose.model('Image', image);
