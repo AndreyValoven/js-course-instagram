308f76574f7694e84fe4ff31378247e54c67d325
AndyScorp <scorpion.and@ukr.net>
2018-05-04 22:24:58 +0300

    crete route for delete following and modified patch followind

--- main/router/user/following.js
+++ main/router/user/following.js
@@ -27,7 +27,7 @@ following.get('/',varyfiToken ,(req, res) => {
     });
 });
 
-
+// need to test
 following.patch('/', varyfiToken,
     (req, res) => {
         let url = req.originalUrl.split('/');
@@ -38,6 +38,7 @@ following.patch('/', varyfiToken,
         User.findById(id, (error, user) => {
             if(user === null) return res.status(404).json({ erorr: 'not ezist'});
             if (error) return res.status(500).json({ error });
+            if (user.followers.includes(req.id)) return res.status(500).json({messge: 'you follow this user'});
             user.followers.push(req.id);
             user.save()
                 .then(user => {
@@ -61,5 +62,42 @@ following.patch('/', varyfiToken,
         });
     }
 );
+
+// need to test
+following.delete('/', varyfiToken, (req, res) => {
+    let url = req.originalUrl.split('/');
+    const id = url[3];
+    if (typeof(req.id) === 'undefined' || req.id + '' === id+ '') {
+        return res.status(403).json({ erorr: 'Forbidden'});
+    }
+    User.findById(id, (error, user) => {
+        if(user === null) return res.status(404).json({ erorr: 'not ezist'});
+        if (error) return res.status(500).json({ error });
+        let index = user.followers.indexOf(req.id);
+        if (index === -1) return res.status(404).jons({ message: 'you not follow user'});
+        delete user.followers[index];
+        user.save()
+            .then(user => {
+                console.log(user);
+                User.findById(req.id, (error, user) => {
+                    if (error) return res.status(500).json({ error });
+                    let index = user.following.indexOf(req.id);
+                    if (index === -1) return res.status(404).jons({ message: 'user not follow you'});
+                    delete user.following[index];
+                    user.save()
+                        .then(user => {
+                            console.log(user);
+                            res.send('ok');
+                        })
+                        .catch(error => {
+                            res.status(501).json({ error });
+                        });
+                });
+            })
+            .catch(error => {
+                res.status(501).json({ error });
+            });
+    });
+});
 
 module.exports = following;
